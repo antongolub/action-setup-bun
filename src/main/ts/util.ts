5a7b46d486adc2bf059889f00c5b1378df78a39f
Anton Golub <antongolub@antongolub.com>
2023-07-11 18:32:06 +0300

    fix: handle fs.rename cross links

--- src/main/ts/util.ts
+++ src/main/ts/util.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import fs from 'fs/promises'
 
 export const parseInput = (value: string): string | undefined => {
   if (value === 'false' || value === '') return undefined
@@ -8,3 +9,14 @@ export const parseInput = (value: string): string | undefined => {
 
 export const getInput = (name: string): string | undefined =>
   parseInput(core.getInput(name))
+
+// https://github.com/antongolub/action-setup-bun/issues/62
+// https://stackoverflow.com/questions/43206198/what-does-the-exdev-cross-device-link-not-permitted-error-mean
+export const rename = async (from: string, to: string) => {
+  try {
+    await fs.rename(from, to)
+  } catch (e) {
+    await fs.cp(from, to)
+    await fs.rm(from)
+  }
+}
